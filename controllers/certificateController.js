9ca82237edb2c7deabbc5f375d716955b3ec80a5
VikingRock <a.zinchuk.a@gmail.com>
2016-10-03 23:26:00 +0300

    link index fixed, parameters added

--- controllers/certificateController.js
+++ controllers/certificateController.js
@@ -18,8 +18,9 @@ var certificate = require('../public/assets/certificate.json');
 var key = fs.readFileSync('./public/assets/drpbx-key.txt', 'utf-8');
 var pathToCertificateFile; //path where uploaded csv is saved to
 var inputFileName; // csv input file name
-var studentsArr = [];
-var sharedLinksArr = [];
+var destinationFolder = '/Training/Training Assesments/Certificates'; //where to save certificates in Dropbox
+var studentsArr;
+var sharedLinksArr;
 
 
 module.exports = function(app) {
@@ -46,7 +47,7 @@ module.exports = function(app) {
 
     };
 
-    var sendEmail = function() {
+    var sendEmail = function(to) {
         var emailText = 'Trainees\' certificates:\n';
 
         studentsArr.forEach(function(item, index) {
@@ -56,9 +57,9 @@ module.exports = function(app) {
 
         server.send({
             text:    emailText,
-            from:    "wiproworker@yandex.ru",
-            to:      "a.zinchuk.a@gmail.com",
-            subject: "WF ceftificate links"
+            from:    'wiproworker@yandex.ru',
+            to:      to,
+            subject: 'WF ceftificate links'
         }, function(err) {
             if (!err) {
                 console.log('email sent');
@@ -73,7 +74,7 @@ module.exports = function(app) {
         });
     };
 
-    var shareDropboxFile = function(path) {
+    var shareDropboxFile = function(path, index) {
         var options = {
             url: 'https://api.dropboxapi.com/2/sharing/create_shared_link_with_settings',
             headers: {
@@ -87,7 +88,7 @@ module.exports = function(app) {
             var info = JSON.parse(body);
             if (!error && response.statusCode == 200) {
                 console.log('Shared URL: ' + info.url);
-                sharedLinksArr.push(info.url);
+                sharedLinksArr[index] = info.url;
             } else {
                 console.log(info.error_summary);
             }
@@ -96,14 +97,14 @@ module.exports = function(app) {
         request.post(options, callback);
     };
 
-    var createDropboxFile = function(path, name) {
+    var createDropboxFile = function(path, name, destination) {
         var myStream = fs.createReadStream(path);
 
         var options = {
             url: 'https://content.dropboxapi.com/2/files/upload',
             headers: {
                 'Authorization': 'Bearer ' + key,
-                'Dropbox-API-Arg': '{\"path\": \"/Test/' + name + '\",\"mode\": \"add\",\"autorename\": true,\"mute\": false}',
+                'Dropbox-API-Arg': '{\"path\": \"' + destination + '/' + name + '\",\"mode\": \"add\",\"autorename\": true,\"mute\": false}',
                 'Content-Type': 'application/octet-stream'
             },
             body: myStream
@@ -117,7 +118,7 @@ module.exports = function(app) {
                 console.log('File Name: ' + info.name);
                 console.log('File Size: ' + info.size);
 
-                shareDropboxFile('/Test/' + name);
+                shareDropboxFile(destination + '/' + name, name[0]);
 
             } else {
                 console.log(info.error_summary);
@@ -127,12 +128,12 @@ module.exports = function(app) {
         request.post(options, callback);
     };
 
-    var convertToPdf = function(pathToHtml, index, timestamp) {
+    var convertToPdf = function(pathToHtml, index, timestamp, company) {
 
         url2pdf.renderPdf(pathToHtml, {saveDir: './uploads/pdf'})
             .then(function(path){
                 console.log('Rendered pdf @', path);
-                createDropboxFile(path, index + timestamp + '.pdf');
+                createDropboxFile(path, index + timestamp + '.pdf', destinationFolder + '/' + company);
             });
     };
 
@@ -149,7 +150,7 @@ module.exports = function(app) {
                     var fileName = './uploads/' + index + timestamp + '.html';
 
                     fs.writeFile(fileName, renderedCertificate, function(err) {
-                        convertToPdf(fileName, index, timestamp);
+                        convertToPdf(fileName, index, timestamp, formFields.company);
                     });
                 });
             })
@@ -178,14 +179,14 @@ module.exports = function(app) {
             csvToJson(pathToCertificateFile, fields);
 
             setTimeout(function() {
-                res.writeHead(200, {'content-type': 'text/plain'});
-                res.write('received upload:\n\n');
-                res.end(util.inspect({fields: fields, files: files}));
+                res.writeHead(200, {'content-type': 'text/html'});
+                res.write('\<p\>Your certifictes have arrived\<\/p\>\<a href="/certificate"\>Go to Homepage\<\/a\>');
+                res.end();
                 console.log('---\nresponse sent');
 
                 deleteTempFiles('./uploads');
-                sendEmail();
-            }, 7000);
+                sendEmail(fields.email);
+            }, 10000);
 
         });
 
